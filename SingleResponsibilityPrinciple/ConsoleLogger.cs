4df9c0bc9f638101f1ea2ff958b289a7702463a6
Owen-Krueger <owk914@gmail.com>
2018-11-02 21:24:21 -0500

    Request 405: Used a StreamWriter to write to a log file (both warnings and info messages).

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
@@ -9,11 +9,19 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            using (StreamWriter w = File.AppendText("log.xml")) //Log information to an xml file
+            {
+                w.WriteLine("<log><type>WARN</type><message>" + message + "</message>", args); //message to write out
+            }
         }
 
         public void LogInfo(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
+            using (StreamWriter w = File.AppendText("log.xml")) //Log information to an xml file
+            {
+                w.WriteLine("<log><type>INFO</type><message>" + message + "</message>", args); //message to write out
+            }
         }
     }
 }
